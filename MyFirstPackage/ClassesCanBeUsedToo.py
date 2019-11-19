11333da6ce49782d5d3ad118bc18226dad228216
kwintenvandebroeck <kwintenvdb@gmail.com>
2019-11-19 14:16:45 +0100

    formatting

--- MyFirstPackage/ClassesCanBeUsedToo.py
+++ MyFirstPackage/ClassesCanBeUsedToo.py
@@ -1,5 +1,7 @@
-#the (object) is the same as extends in Java. It implies an "is-a" relationship
-#this can be removed and simply writen as "class Person:" if object is the super
+# the (object) is the same as extends in Java. It implies an "is-a" relationship
+# this can be removed and simply writen as "class Person:" if object is the super
+
+
 class Person(object):
 
     def __init__(self, name):
@@ -12,15 +14,16 @@ class Person(object):
 myself = Person("Kwinten")
 myself.print_name()
 
-#default arguments are a thing but not a singleton. It only gets initialised once and then reused.
-def extendList(val, list=[]):
+
+# default arguments are a thing but not a singleton. It only gets initialised once and then reused.
+def extend_list(val, list=[]):
     list.append(val)
     return list
 
 
-list1 = extendList(10)
-list2 = extendList(123,[])
-list3 = extendList('a')
+list1 = extend_list(10)
+list2 = extend_list(123, [])
+list3 = extend_list('a')
 
 print("list1 = %s" % list1)
 print("list2 = %s" % list2)
