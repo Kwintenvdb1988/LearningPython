c6e4281b64f1a73eaf5561fead03299f783a67a1
kwintenvandebroeck <kwintenvdb@gmail.com>
2019-11-19 14:15:33 +0100

    default arguments are a thing here, that can be confusing

--- MyFirstPackage/ClassesCanBeUsedToo.py
+++ MyFirstPackage/ClassesCanBeUsedToo.py
@@ -1,3 +1,5 @@
+#the (object) is the same as extends in Java. It implies an "is-a" relationship
+#this can be removed and simply writen as "class Person:" if object is the super
 class Person(object):
 
     def __init__(self, name):
@@ -9,3 +11,17 @@ class Person(object):
 
 myself = Person("Kwinten")
 myself.print_name()
+
+#default arguments are a thing but not a singleton. It only gets initialised once and then reused.
+def extendList(val, list=[]):
+    list.append(val)
+    return list
+
+
+list1 = extendList(10)
+list2 = extendList(123,[])
+list3 = extendList('a')
+
+print("list1 = %s" % list1)
+print("list2 = %s" % list2)
+print("list3 = %s" % list3)
